32615e9e072eab6a4edb807ca4e1db0405566a18
Janine Luk <janineluk95@gmail.com>
2018-11-21 21:57:43 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    array = self.split(/\.?\s+/)
+    array = self.split(/[.?!]/)
     array.delete_if{|element| element.empty?}
     array.length
   end
